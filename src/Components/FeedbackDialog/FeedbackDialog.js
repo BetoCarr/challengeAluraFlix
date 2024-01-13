5c1f78596fd09cd9f8b23807653aba5c28658c49
Beto Carrillo <beto.valo59@gmail.com>
2024-01-13 15:43:55 -0600

    Añadí el componente FeedbackDialog y lo implementé en los componentes LikeIcon y DeleteForeverIcon para proporcionar una retroalimentación consistente y mejorar la experiencia del usuario.

--- src/Components/FeedbackDialog/FeedbackDialog.js
+++ src/Components/FeedbackDialog/FeedbackDialog.js
@@ -1,24 +1,36 @@
+import './FeedbackDialog.css'
 import React from 'react';
 import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import Button from '@mui/material/Button';
 
-function FeedbackDialog({ isOpen, onClose, message, onConfirm, confirmLabel }) {
-
+function FeedbackDialog({ isOpen, onClose, message, onCancel, onConfirm, cancelLabel, confirmLabel }) {
+    
+    // Función para manejar el cierre del cuadro de diálogo
     function handleClose() {
         onClose();
     }
 
     return (
+        // Componente de cuadro de diálogo de Material-UI
         <Dialog open={isOpen} onClose={handleClose}>
+            {/* Contenido del cuadro de diálogo */}
             <DialogContent className='box-dialog'>
-                {message}
+                {message} {/* Muestra el mensaje proporcionado */}
             </DialogContent>
+            {/* Acciones del cuadro de diálogo */}
             <DialogActions>
+                {/* Botón de cancelación, si la función onCancel está definida */}
+                {onCancel && (
+                    <Button onClick={onCancel} color="primary">
+                        {cancelLabel || 'Cancelar'}
+                    </Button>
+                )}
+                {/* Botón de confirmación, si la función onConfirm está definida */}
                 {onConfirm && (
                     <Button onClick={onConfirm} color="primary">
-                        {confirmLabel || 'Ok'}
+                        {confirmLabel || 'Aceptar'}
                     </Button>
                 )}
             </DialogActions>
