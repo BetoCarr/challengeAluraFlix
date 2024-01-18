8a21379a781a40e4647b84635ba0946c7cc0f545
Beto Carrillo <beto.valo59@gmail.com>
2024-01-18 08:05:57 -0600

    Agregué comentarios a los componentes modificados

--- src/Components/Carousel/Slider/Slider.js
+++ src/Components/Carousel/Slider/Slider.js
@@ -1,19 +1,23 @@
+// Importa React y los estilos del componente Slider
 import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./StyleSlider.css"
 
-
+// Definición del componente funcional MySlider
 function MySlider ({ children }) {
+
+    // Configuración de las opciones del slider usando el paquete react-slick
     const settings = {
-        dots: true,
-        infinite: children.length > 3,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1
+        dots: true, // Muestra los puntos indicadores de posición
+        infinite: children.length > 3, // Permite el desplazamiento infinito si hay más de 3 elementos
+        speed: 500, // Velocidad de animación en milisegundos
+        slidesToShow: 3, // Número de elementos que se muestran a la vez
+        slidesToScroll: 1 // Número de elementos que se desplazan al hacer clic en las flechas
     };
 
+    // Retorna la estructura del componente
     return (
         <div className="slider-container ">
             <Slider {...settings} className="custom-slider">
@@ -23,4 +27,5 @@ function MySlider ({ children }) {
     );
 }
 
+// Exporta el componente MySlider para su uso en otras partes de la aplicación
 export default MySlider;
