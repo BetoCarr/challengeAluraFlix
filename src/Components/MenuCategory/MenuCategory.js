08a0f3aa5b9267d9aa530268bd47210058e7b0c9
Beto Carrillo <beto.valo59@gmail.com>
2024-01-19 18:05:25 -0600

    wip

--- src/Components/MenuCategory/MenuCategory.js
+++ src/Components/MenuCategory/MenuCategory.js
@@ -1,15 +1,59 @@
 // Importación de Reac y componentes
-import React, { useState } from 'react';
-import Menu from '@mui/material/Menu';
+// import './MenuCategory.css'
+import * as React from 'react';
+import { useState } from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import Menu  from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import IconButton from '@mui/material/IconButton';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+
+const StyledMenu = styled((props) => (
+    <Menu
+        elevation={0}
+        anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+        }}
+        transformOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+        }}
+        {...props}
+    />
+))(({ theme }) => ({
+    '& .MuiPaper-root': {
+        borderRadius: 6,
+        marginTop: theme.spacing(1),
+        minWidth: 180,
+        width:100,
+        backgroundColor: theme.palette.primary.main, // Cambia el color de fondo del menú
+        color: theme.palette.text.primary, // Cambia el color de texto del menú
+        // Otros estilos que desees aplicar al menú
+    },
+    '& .MuiMenuItem-root': {
+        '& .MuiSvgIcon-root': {
+            fontSize: 18,
+            color: theme.palette.text.secondary,
+            marginRight: theme.spacing(1.5),
+        },
+    '&:active': {
+        backgroundColor: alpha(
+            theme.palette.primary.main,
+            theme.palette.action.selectedOpacity,
+        ),
+    },
+      // Otros estilos que desees aplicar a los elementos del menú
+    },
+}));
+
 
 // Componente principal MenuCategory
 function MenuCategory ({ onDelete }) {
     // Estado para gestionar la apertura y cierre del menú
     const [anchorEl, setAnchorEl] = useState(null);
-    
+
     // Manejador de clic para abrir el menú
     const handleClick = (event) => {
         setAnchorEl(event.currentTarget);
@@ -31,25 +75,31 @@ function MenuCategory ({ onDelete }) {
         <>
             {/* Botón de menú con el icono de tres puntos verticales */}
             <IconButton
+                className='menu-icon-button'
                 aria-label="more"
                 aria-controls="menu-categoria"
                 aria-haspopup="true"
                 onClick={handleClick}
             >
-                <MoreVertIcon />
+                <KeyboardArrowDownIcon />
             </IconButton>
             {/* Menú desplegable */}
-            <Menu
-                id="menu-categoria"
+            <StyledMenu
+                id="demo-customized-menu"
+                MenuListProps={{
+                    'aria-labelledby': 'demo-customized-button',
+                }}
                 anchorEl={anchorEl}
-                keepMounted
                 open={Boolean(anchorEl)}
                 onClose={handleClose}
             >
                 {/* Elemento de menú para la opción "Eliminar" */}
-                <MenuItem onClick={handleMenuDelete}>Eliminar</MenuItem>
+                <MenuItem className='menu-item' onClick={handleMenuDelete}>
+                    <DeleteForeverIcon color="primary" fontSize='large'/>
+                    Eliminar
+                </MenuItem>
                 {/* Puedes agregar más elementos del menú aquí */}
-            </Menu>
+            </StyledMenu>
         </>
     );
 }
