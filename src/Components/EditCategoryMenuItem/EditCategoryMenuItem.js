5a6d0ccb1845477913a324746178c9414c14129b
Beto Carrillo <beto.valo59@gmail.com>
2024-02-06 16:25:35 -0600

    wip

--- src/Components/EditCategoryMenuItem/EditCategoryMenuItem.js
+++ src/Components/EditCategoryMenuItem/EditCategoryMenuItem.js
@@ -1,6 +1,7 @@
 import React, { useState }  from 'react';
 import FormEditarCategoria from '../FormEditarCategoria/FormEditarCategoria';
 import ManageCategoryMenuItem from '../ManageCategoryMenuItem/ManageCategoryMenuItem';
+import { handleCloseDialog } from '../ManageCategoryMenuItem/ManageCategoryMenuItem';
 
 function EditCategroyMenuItem({ categoryId, categoryName, categoryColor, isBanner, handleClose }) {
     // Estado para controlar la visibilidad del formulario de edición
@@ -13,14 +14,24 @@ function EditCategroyMenuItem({ categoryId, categoryName, categoryColor, isBanne
         isBanner: isBanner
     }
 
-    const handleEditCategory = () => {
-        handleEditClick();
-        
-    }
+    // // Función para manejar la operación de edición en el backend
+    // const handleEditOperation = async (editedCategory) => {
+    //     try {
+    //         // Realizar la solicitud al backend con la categoría editada
+    //         // await yourBackendEditCategoryFunction(categoryId, editedCategory);
+    //         // Cerrar el formulario de edición después de completar la operación
+    //         handleEditClose();
+    //     } catch (error) {
+    //         console.error('Error al editar la categoría:', error);
+    //         // Manejar el error si ocurre
+    //         // Puedes mostrar un mensaje de error al usuario si lo deseas
+    //     }
+    // };
 
     // Función para manejar la apertura del formulario de edición
     const handleEditClick = () => {
         setShowEditForm(true);
+        handleCloseDialog();
     };
 
     // Función para manejar el cierre del formulario de edición
@@ -35,13 +46,15 @@ function EditCategroyMenuItem({ categoryId, categoryName, categoryColor, isBanne
                 categoryId={categoryId}
                 categoryName={categoryName}
                 handleClose={handleClose}
-                handleBackendOperation={handleEditCategory}
+                handleBackendOperation={handleEditClick}
             />
             {/* Renderiza el formulario de edición condicionalmente */}
             {showEditForm && (
                 <FormEditarCategoria
                     initialValuesForEdit={initialValuesForEdit}
                     handleClose={handleEditClose}
+                    // Pasa handleEditOperation como prop al formulario de edición
+                    // handleEditOperation={handleEditOperation}
                 />
             )}
         </>
