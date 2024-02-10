377dd51d3288e40d398bc6bad55a2b2c29ccca0c
Beto Carrillo <beto.valo59@gmail.com>
2024-02-09 18:46:16 -0600

    Tamanio de letras de botones arreglados

--- src/Components/FormButtons/FormButtons.js
+++ src/Components/FormButtons/FormButtons.js
@@ -5,15 +5,37 @@ import { Box, Button } from '@mui/material';
 function FormButtons ({ isSubmitting, resetForm }) {
     return(
         <Box className='container-botones'>
-            <Box className='container-botones-izquierda'>
-                <Button type="submit" disabled={isSubmitting} size='large' variant='outlined' className='boton-azul'>
-                    Guardar
-                </Button>
-                <Button  disabled={isSubmitting} size='large' variant='outlined' className='boton-gris' type='button' onClick={() => resetForm()}>
-                    Limpiar
-                </Button>
-            </Box>
+            <Button 
+                type='submit' 
+                disabled={isSubmitting} 
+                size='large' 
+                variant='outlined' 
+                sx={{
+                    width: '47%',
+                    backgroundColor: 'var(--boton-blue)',
+                    color: 'var(--text-primary)',
+                    fontSize: '75%'
+                }}
+            >
+                Guardar
+            </Button>
+            <Button  
+                disabled={isSubmitting} 
+                size='large' 
+                variant='outlined' 
+                type='button' 
+                onClick={() => resetForm()}
+                sx={{
+                    width: '47%',
+                    backgroundColor: 'var(--boton-gray)',
+                    color: 'var(--text-primary)',
+                    fontSize: '75%'
+                }}
+            >
+                Limpiar
+            </Button>
         </Box>
+        
     );
 }
 
