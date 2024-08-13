8ec93b3736150a24c23034ea513997a7175a5fb8
Beto Carrillo <beto.valo59@gmail.com>
2024-08-13 09:57:40 -0600

    wip

--- src/features/videocategories/deleteCategoryListener.js
+++ src/features/videocategories/deleteCategoryListener.js
@@ -1,6 +1,6 @@
 import { createListenerMiddleware } from "@reduxjs/toolkit";
 import { deleteCategory } from "./videoCategoriesSlice";
-import { showFeedbackToUser } from "../feedbackdialog/feedbackActions";
+import { showFeedbackToUser, closeFeedback } from "../feedbackdialog/feedbackActions";
 
 // Crea el middleware de listener
 const listenerMiddleware = createListenerMiddleware();
@@ -10,7 +10,7 @@ listenerMiddleware.startListening({
     effect: async (action, listenerApi) => {
         listenerApi.dispatch(showFeedbackToUser({
             message: 'Eliminando categoría...',
-            confirmLabel: 'Aceptar',
+            // confirmLabel: 'Aceptar',
         }));
     },
 });
@@ -18,10 +18,14 @@ listenerMiddleware.startListening({
 listenerMiddleware.startListening({
     actionCreator: deleteCategory.fulfilled, // Intercepta cuando el thunk se completa exitosamente
     effect: async (action, listenerApi) => {
+        await listenerApi.condition((action) => action.type === closeFeedback.type);
         listenerApi.dispatch(showFeedbackToUser({
             message: '¡Categoría eliminada exitosamente!',
             confirmLabel: 'Aceptar',
         }));    
+         // Espera a que el feedback sea cerrado antes de continuar
+         // Aquí se puede despachar una acción para actualizar el estado de Redux o ejecutar cualquier otra lógica.
+        console.log('Feedback cerrado, ahora puedes actualizar el estado.');
     },
 });
 
