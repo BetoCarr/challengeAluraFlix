7765ab9c32939deddef63d2e7e6f49facdcb4a48
Beto Carrillo <beto.valo59@gmail.com>
2024-11-18 18:57:02 -0600

    HomeSkeleton terminado

--- src/Components/HomePageSkeleton/HomePageSkeleton.js
+++ src/Components/HomePageSkeleton/HomePageSkeleton.js
@@ -1,28 +1,39 @@
+// Importación de React y componentes
 import React from 'react';
 import './HomePageSkeleton.css'
-
 import { Skeleton, Box, Grid } from '@mui/material';
 
+// Componente para mostrar un esqueleto de carga en la página Home
 function HomePageSkeleton() {
     return (
+        // Contenedor principal del esqueleto
         <Box  className='skeleton-container'>
-            <Box>
-                <Skeleton variant="rectangular" width="18%" height={70} sx={{ marginBottom: '16px' }} />
-                <Skeleton variant="rectangular" width="25%" height={90} sx={{ marginBottom: '16px' }} />
-
+            {/* Contenedor para el título y el contenido principal */}
+            <Box className='title-container'>  
+                {/* Contenido del lado izquierdo (título y descripción) */}
+                <Box className='left-content'>
+                    <Skeleton variant="rectangular" width="45%" height={70} sx={{ marginBottom: '16px' }} />
+                    <Skeleton variant="rectangular" width="95%" height={100} sx={{ marginBottom: '16px' }} />
+                </Box>
+                {/* Contenido del lado derecho (elemento destacado, por ejemplo, imagen o banner) */}
+                <Box className='right-content'>
+                    <Skeleton variant="rectangular" width="100%" height={280} sx={{ marginBottom: '16px' }} />
+                </Box>
             </Box>
-            <Box>
+            {/* Contenedor para las tarjetas (videos o contenido relacionado) */}
+            <Box className='cards-container'>
                 <Grid container spacing={2}>
                     {[1, 2, 3].map((_, index) => (
-                        <Grid item xs={12} sm={6} md={3} key={index}>
-                            <Skeleton variant="rectangular" width="100%" height={180} />
+                        <Grid item xs={12} sm={6} md={4} key={index}>
+                            <Skeleton 
+                                variant="rectangular" 
+                                width="100%" 
+                                height={250} 
+                            />
                         </Grid>
                     ))}
                 </Grid>
-            </Box>
-            
-            {/* Video Cards Skeleton */}
-        
+            </Box>        
         </Box>
     );
 }
