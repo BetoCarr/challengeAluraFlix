b2f9fed71453440a74280905c0f040763b8923cb
Beto Carrillo <beto.valo59@gmail.com>
2024-11-20 08:25:18 -0600

    TitleContainer revisado

--- src/features/videocategories/components/TitleContainer/TitleContainer.js
+++ src/features/videocategories/components/TitleContainer/TitleContainer.js
@@ -9,20 +9,20 @@ import { selectCategoryById } from '../../videoCategoriesSlice';
 
 // Componente que representa un contenedor de título con un menú desplegable
 function TitleContainer({ categoryId }) {
+
+    // Obtiene categoria y propiedades de la categoria
     const category = useSelector(state => selectCategoryById(state, categoryId));
     const { nombre, color, isBanner } = category;
 
-    // console.log(category)
-
     // Determina la clase de estilo basada en si es un banner o no
-    const className = isBanner ? 'paper-banner' : 'paper';
-    const typographyClassName = isBanner ? 'typography-banner' : 'typography';
+    const paperClassName = isBanner ? 'paper-banner' : 'paper';
+    const titleClassName = isBanner ? 'typography-banner' : 'typography';
 
     return(
         // Contenedor de Material-UI con sombra y color de fondo personalizado
-        <Paper elevation={2} style={{backgroundColor: color}} className={className}>
+        <Paper elevation={2} style={{backgroundColor: color}} className={paperClassName}>
             {/* Texto del título con estilo de encabezado h4 */}
-            <Typography variant='h4' className={typographyClassName}>
+            <Typography variant='h4' className={titleClassName}>
                 {nombre}
             </Typography>
             {/* Componente del menú desplegable para acciones en la categoría */}
